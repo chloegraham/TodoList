5b52617237c1d1cfb1883ea6ce5648ccd9bf091d
Chloe <chloe.graham10@gmail.com>
2014-11-22 17:20:40 +1300

    initial commit

---
+++ app/src/main/java/chloe/model/Attachment.java
@@ -0,0 +1,7 @@
+package chloe.model;
+
+/**
+ * Created by Chloe on 22/11/2014.
+ */
+public class Attachment extends AbstractModel {
+}
---
+++ app/src/main/java/chloe/model/CheckItem.java
@@ -0,0 +1,7 @@
+package chloe.model;
+
+/**
+ * Created by Chloe on 22/11/2014.
+ */
+public class CheckItem extends AbstractModel {
+}
